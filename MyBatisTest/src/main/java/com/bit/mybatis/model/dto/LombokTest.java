8eb8a5bebd3722b2bb80bc9c6710cbb28bc3753a
ohjuah <ohjuah0119@naver.com>
2021-04-20 19:37:53 +0900

    Committtt

---
+++ MyBatisTest/src/main/java/com/bit/mybatis/model/dto/LombokTest.java
@@ -0,0 +1,14 @@
+package com.bit.mybatis.model.dto;
+
+import lombok.Data;
+
+
+@Data
+public class LombokTest {
+	private String id;
+	private String title;
+	private String content;
+	private int no;
+	
+	
+}
---
+++ MyBatisTest/src/main/java/use/Test.java
@@ -0,0 +1,17 @@
+package use;
+
+import com.bit.mybatis.model.dto.LombokTest;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LombokTest test = new LombokTest();
+		test.setNo(123);
+		test.setId("id여라");
+		System.out.println(test);
+		
+		
+	}
+
+}
